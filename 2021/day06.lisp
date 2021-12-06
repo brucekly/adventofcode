b3f76b94e0ea53160dcc2dbd6f65185d78f0f2bd
Lucy McPhail <lucy.mcphail.p@gmail.com>
2021-12-06 20:55:17 +0000

    Clean day 6

--- 2021/day06.lisp
+++ 2021/day06.lisp
@@ -1,37 +1,24 @@
 (require "asdf")
-(ql:quickload "cl-ppcre")
 
 (defparameter *input* (uiop:read-file-line "input/day06.txt"))
 
 (defun str-to-ints (str)
-  (mapcar #'parse-integer (cl-ppcre:all-matches-as-strings "\\d+" str)))
+  (mapcar #'parse-integer (uiop:split-string str :separator ",")))
 
-(defun ints-to-count (ints)
-  (let ((fish (make-hash-table)))
-    (loop for n in ints
-	  do (incf (gethash n fish 0))
-	  finally (return fish))))
+(defun counts (ints)
+  (loop for n from 0 to 8
+	collect (count n ints)))
 
-(defun advance-fishes (fishes)
-  (let ((new-fish (make-hash-table)))
-    (loop for k being the hash-keys in fishes using (hash-value v)
-	  if (zerop k) do
-	    (incf (gethash 6 new-fish 0) v)
-	    (incf (gethash 8 new-fish 0) v)
-	  else do
-	    (incf (gethash (1- k) new-fish 0) v)
-	  finally (return new-fish))))
+(defun advance (fishes)
+  (let ((next (append (cdr fishes) (list (car fishes)))))
+    (incf (nth 6 next) (nth 8 next))
+    next))
 
 (defun simulate-fishes (times input)
-  (loop for n from 0 to times
-	for fish = input
-	  then (advance-fishes fish)
+  (loop repeat (1+ times)
+	for fish = input then (advance fish)
 	finally (return fish)))
 
-(defun count-fishes (fishes)
-  (loop for v being the hash-values in fishes
-	sum v))
-
-(let ((initial-fishes (ints-to-count (str-to-ints *input*))))
-  (print (count-fishes (simulate-fishes 80 initial-fishes)))
-  (print (count-fishes (simulate-fishes 256 initial-fishes))))
+(let ((initial (counts (str-to-ints *input*))))
+  (print (apply #'+ (simulate-fishes 80 initial)))
+  (print (apply #'+ (simulate-fishes 256 initial))))
